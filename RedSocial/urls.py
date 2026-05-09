a6209cfa73ea17bba9561d896b36bcc097771c95
tatomesa <seba_mesa@hotmail.com>
2026-05-09 23:37:01 +0000

    arreglando urls

--- RedSocial/urls.py
+++ RedSocial/urls.py
@@ -38,3 +38,6 @@ urlpatterns = [
     path('contact/', ContactView.as_view(), name ='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

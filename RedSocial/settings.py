0a3c9312c87bfdd467ae008b9ee650516e7de958
tatomesa <seba_mesa@hotmail.com>
2026-05-10 19:06:47 +0000

    fix para que whitenoise encuentre los css del admin

--- RedSocial/settings.py
+++ RedSocial/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+WHITENOISE_KEEP_ONLY_HASHED_FILES = False
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
